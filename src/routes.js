34ed4227a205f5a0e66394b5d233c9a8716e2c71
xuanhewang <811300735@qq.com>
2017-06-21 17:21:03 +0800

    react路由

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
-import React, { PropTypes } from 'react'
-import { Router, Route, hashHistory ,browserHistory, IndexRoute} from 'react-router'
+import React, {PropTypes} from 'react'
+import {Router, Route, hashHistory, browserHistory, IndexRoute} from 'react-router'
 
 
 import App from './App'
@@ -7,19 +7,21 @@ import Home from './components/Home/Home'
 import Blog from './components/Blog/Blog'
 import Work from './components/Work/Work'
 import About from './components/About/About'
+import Item from './components/Blog/Item'
 class Routers extends React.Component {
-  render () {
-    return(
-      <Router history={hashHistory}>
-        <Route path='/' component={App}>
-          <IndexRoute component={Home}/>
-          <Route path='blog' component={Blog} />
-          <Route path='work' component={Work} />
-          <Route path='about' component={About} />
-        </Route>
-      </Router>
-    )
-  }
+    render() {
+        return (
+            <Router history={hashHistory}>
+              <Route path='/' component={App}>
+                <IndexRoute component={Home}/>
+                <Route path='blog' component={Blog}/>
+                <Route path='work' component={Work}/>
+                <Route path='about' component={About}/>
+                <Route path='blog/:url' component={Item}/>
+              </Route>
+            </Router>
+        )
+    }
 }
 
 export default Routers;
